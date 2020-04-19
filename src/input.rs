e52524c0a0359ac2c7225cb935b8bdcf44c2f49f
RadoslawWarisch <radoslaw.warisch@brainly.com>
2020-04-19 19:41:22 +0200

    [0.0.2] Case sensitivity by env var added

--- src/input.rs
+++ src/input.rs
@@ -4,29 +4,42 @@ use std::env;
 pub struct CmdInput {
     pub path: String,
     pub query: String,
+    pub is_case_sensitive: bool,
 }
 
 pub trait NewCmdInput {
-    fn new(args: &[String]) -> Result<CmdInput, &'static str>;
+    fn new(
+        args: &[String],
+        env_var: &Result<String, env::VarError>,
+    ) -> Result<CmdInput, &'static str>;
 }
 
 impl NewCmdInput for CmdInput {
-    fn new(args: &[String]) -> Result<CmdInput, &'static str> {
+    fn new(
+        args: &[String],
+        env_var: &Result<String, env::VarError>,
+    ) -> Result<CmdInput, &'static str> {
         if args.len() < 3 {
             return Err("No enough input");
         }
 
         let path = args[1].clone();
         let query = args[2].clone();
+        let is_case_sensitive = !env_var.is_err();
 
-        Ok(CmdInput { path, query })
+        Ok(CmdInput {
+            path,
+            query,
+            is_case_sensitive,
+        })
     }
 }
 
 pub fn get_cmd_input() -> Result<CmdInput, &'static str> {
     let args: Vec<String> = env::args().collect();
+    let env_var = env::var("CASE_SENSITIVE");
 
-    CmdInput::new(&args)
+    CmdInput::new(&args, &env_var)
 }
 
 #[cfg(test)]
@@ -34,33 +47,27 @@ mod test {
     use super::*;
 
     fn cmd_input_mock(args: &[&str]) -> Result<CmdInput, &'static str> {
-        let mock_cmd_raw_input = args
+        let mock_args = args
             .iter()
             .map(|&slice| String::from(slice))
             .collect::<Vec<String>>();
 
-        CmdInput::new(&mock_cmd_raw_input)
+        let mock_env_var = Ok(String::from("its okay"));
+
+        CmdInput::new(&mock_args, &mock_env_var)
     }
 
     #[test]
     fn cmd_input_success() {
         let args = &vec!["", "./text.txt", "hello"];
 
-        let mock_cmd_input = cmd_input_mock(args);
-
-        if let Err(err) = mock_cmd_input {
-            panic!("Should return a Ok result instead of {:#?}", err);
-        }
+        assert!(cmd_input_mock(args).is_ok());
     }
 
     #[test]
     fn cmd_input_fail() {
         let args = &vec![""];
 
-        let mock_cmd_input = cmd_input_mock(args);
-
-        if let Ok(res) = mock_cmd_input {
-            panic!("Should return a Err result instead of {:#?}", res);
-        }
+        assert!(cmd_input_mock(args).is_err());
     }
 }
--- src/search.rs
+++ src/search.rs
@@ -6,12 +6,22 @@ fn get_file_content(input: &CmdInput) -> Result<String, Box<dyn Error>> {
     Ok(fs::read_to_string(&input.path[..])?)
 }
 
+fn is_query_in_content(content: String, input: &CmdInput) -> bool {
+    let query = &input.query[..];
+
+    if input.is_case_sensitive {
+        return content.contains(query);
+    } else {
+        return content.to_lowercase().contains(&query.to_lowercase()[..]);
+    }
+}
+
 pub fn search_for_query(input: &CmdInput) -> Result<bool, Box<dyn Error>> {
     let file_content = get_file_content(input);
 
     match file_content {
         Ok(content) => {
-            return Ok(content.contains(&input.query[..]));
+            return Ok(is_query_in_content(content, input));
         }
         Err(err) => Err(err),
     }
